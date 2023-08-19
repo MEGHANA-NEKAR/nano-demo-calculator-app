dada08b50900d72bd8c0d0da93c7807b6b03ba30
N Meghana <nmeghana195@gmail.com>
2023-08-19 22:31:15 +0530

    trail 1

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first + second
+        response = {'result': result}
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'Invalid input format'})
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json
+    if 'first' in data and 'second' in data:
+        first = data['first']
+        second = data['second']
+        result = first - second
+        response = {'result': result}
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'Invalid input format'})
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
+
